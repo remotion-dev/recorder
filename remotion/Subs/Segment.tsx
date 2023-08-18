7d76ab03a8d68c7b3fef121df8ab852031f2387f
Jonny Burger <jonathanburger11@gmail.com>
2023-08-18 19:09:48 +0200

    awesome

--- remotion/Subs/Segment.tsx
+++ remotion/Subs/Segment.tsx
@@ -1,13 +1,23 @@
 import React from "react";
-import { AbsoluteFill } from "remotion";
+import { AbsoluteFill, useVideoConfig } from "remotion";
 import type { Segment } from "../sub-types";
 import { useTime, WordComp } from "./Word";
+
+const getFontSize = (height: number) => {
+  if (height < 1000) {
+    return 28;
+  }
+
+  return 36;
+};
 
 export const SegmentComp: React.FC<{
   segment: Segment;
   isLast: boolean;
-}> = ({ segment, isLast }) => {
-  const time = useTime();
+  trimStart: number;
+}> = ({ segment, isLast, trimStart }) => {
+  const { height } = useVideoConfig();
+  const time = useTime(trimStart);
 
   if (time < segment.start) {
     return null;
@@ -23,7 +33,7 @@ export const SegmentComp: React.FC<{
         top: "auto",
         height: "auto",
         textAlign: "center",
-        fontSize: 28,
+        fontSize: getFontSize(height),
         paddingBottom: 50,
         fontFamily: "GT Planar",
         fontWeight: 500,
@@ -31,7 +41,7 @@ export const SegmentComp: React.FC<{
       }}
     >
       {segment.words.map((word) => {
-        return <WordComp key={word.start} word={word} />;
+        return <WordComp key={word.start} trimStart={trimStart} word={word} />;
       })}
     </AbsoluteFill>
   );
--- remotion/Subs/Word.tsx
+++ remotion/Subs/Word.tsx
@@ -6,20 +6,37 @@ const style: React.CSSProperties = {
   whiteSpace: "pre-wrap",
   display: "inline-block",
 };
-export const useTime = () => {
+export const useTime = (trimStart: number) => {
   const frame = useCurrentFrame();
   const { fps } = useVideoConfig();
-  const time = frame / fps;
+  const time = (frame + trimStart) / fps;
 
   return time;
 };
 
 export const WordComp: React.FC<{
   word: Word;
-}> = ({ word }) => {
-  const time = useTime();
+  trimStart: number;
+}> = ({ word, trimStart }) => {
+  const time = useTime(trimStart);
 
   const opacity = word.start <= time ? 1 : 0.3;
 
-  return <div style={{ ...style, opacity }}>{word.word}</div>;
+  const monospace =
+    word.word.trimStart().startsWith("`") && word.word.endsWith("`");
+
+  const withoutBackticks = word.word.replace(/`/g, "");
+
+  return (
+    <div
+      style={{
+        ...style,
+        opacity,
+        color: monospace ? "#0b84f3" : "black",
+        fontWeight: monospace ? 400 : 500,
+      }}
+    >
+      {withoutBackticks}
+    </div>
+  );
 };
