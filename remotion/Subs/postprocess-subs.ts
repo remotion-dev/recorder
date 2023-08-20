a31dd2615ba8ae1ddfe0835e4b6bdab5f7232b5a
Jonny Burger <jonathanburger11@gmail.com>
2023-08-20 15:42:30 +0200

    postprocess

---
+++ remotion/Subs/postprocess-subs.ts
@@ -0,0 +1,34 @@
+import type { SubTypes, Word } from "../sub-types";
+
+const wordsTogether = (words: Word[]) => {
+  const newWords: Word[] = [];
+
+  for (let i = 0; i < words.length; i++) {
+    const word = words[i];
+    const previousWord = words[i - 1] ?? null;
+
+    if (!word.word.startsWith(" ") && previousWord) {
+      const lastAddedWord = newWords[newWords.length - 1];
+      lastAddedWord.word += word.word;
+      lastAddedWord.end = word.end;
+    } else {
+      newWords.push(word);
+    }
+  }
+
+  return newWords;
+};
+
+export const postprocessSubtitles = (subTypes: SubTypes): SubTypes => {
+  return {
+    ...subTypes,
+    segments: subTypes.segments.map((segment) => {
+      const { words } = segment;
+
+      return {
+        ...segment,
+        words: wordsTogether(words),
+      };
+    }),
+  };
+};
