3f88037b79f444e5699d4ad393323d0269c8f9e3
Jonny Burger <jonathanburger11@gmail.com>
2023-08-22 15:26:44 +0200

    subtitle translarion

--- remotion/animations/camera-scene-transitions.ts
+++ remotion/animations/camera-scene-transitions.ts
@@ -43,3 +43,28 @@ export const getWebcamTranslation = ({
   const translationY = interpolate(enter, [0, 1], [initialPosition, 0]);
   return { translationX, translationY };
 };
+
+export const getSubtitleTranslation = ({
+  enter,
+  exit,
+  width,
+  height,
+  webcamPosition,
+}: {
+  enter: number;
+  exit: number;
+  width: number;
+  height: number;
+  webcamPosition: WebcamPosition;
+}) => {
+  const initialPosition =
+    webcamPosition === "top-left" ||
+    webcamPosition === "top-right" ||
+    webcamPosition === "center"
+      ? -height
+      : height;
+
+  const translationX = interpolate(exit, [0, 1], [0, -width]);
+  const translationY = interpolate(enter, [0, 1], [initialPosition, 0]);
+  return { translationX, translationY };
+};
