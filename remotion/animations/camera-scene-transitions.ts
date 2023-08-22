3a2fb00c81a6b4c30d83fb2d21c010201fe7ad9c
Jonny Burger <jonathanburger11@gmail.com>
2023-08-22 16:22:17 +0200

    dirty transition

--- remotion/animations/camera-scene-transitions.ts
+++ remotion/animations/camera-scene-transitions.ts
@@ -23,30 +23,46 @@ export const getDisplayTranslation = ({
   exit: number;
   width: number;
   previous: { scene: SceneType; metadata: SceneMetadata } | null;
-  scene: { scene: SceneType; metadata: SceneMetadata } | null;
+  scene: { scene: SceneType; metadata: SceneMetadata };
   next: { scene: SceneType; metadata: SceneMetadata } | null;
   canvasSize: Dimensions;
   canvasLayout: CanvasLayout;
 }) => {
   const enterX = interpolate(enter, [0, 1], [width, 0]);
 
-  const exitEndX =
+  const currentLayout =
+    scene.scene.type === "scene"
+      ? getLayout({
+          canvasLayout,
+          canvasSize,
+          display: scene.metadata.videos?.display as Dimensions,
+          webcam: scene.metadata.videos?.webcam as Dimensions,
+          webcamPosition: scene.scene.webcamPosition,
+        }).displayLayout
+      : null;
+
+  const nextLayout =
     next?.scene?.type === "scene"
-      ? (
-          getLayout({
-            canvasLayout,
-            canvasSize,
-            display: next.metadata.videos?.display as Dimensions,
-            webcam: next.metadata.videos?.webcam as Dimensions,
-            webcamPosition: next.scene.webcamPosition,
-          }).displayLayout as Layout
-        ).x
-      : -width;
+      ? (getLayout({
+          canvasLayout,
+          canvasSize,
+          display: next.metadata.videos?.display as Dimensions,
+          webcam: next.metadata.videos?.webcam as Dimensions,
+          webcamPosition: next.scene.webcamPosition,
+        }).displayLayout as Layout)
+      : null;
+
+  const exitEndX =
+    currentLayout && nextLayout ? nextLayout.x - currentLayout.x : -width;
+
+  const exitEndY =
+    currentLayout && nextLayout ? nextLayout.y - currentLayout.y : 0;
 
   const exitX = interpolate(exit, [0, 1], [0, exitEndX]);
+  const exitY = interpolate(exit, [0, 1], [0, exitEndY]);
 
-  const translationX = enterX + exitX + "px 0";
-  const translationY = 0;
+  const translationX = enterX + exitX;
+  const translationY = exitY;
 
   return { translationX, translationY };
 };
