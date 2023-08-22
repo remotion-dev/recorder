0ba11dc8789e1a039ac717ad1b2a1431987c40bc
Jonny Burger <jonathanburger11@gmail.com>
2023-08-22 14:56:46 +0200

    whoosh

--- remotion/scenes/TitleCard.tsx
+++ remotion/scenes/TitleCard.tsx
@@ -1,18 +1,23 @@
 import React from "react";
 import {
   AbsoluteFill,
+  Audio,
+  Img,
   interpolate,
+  Sequence,
   spring,
+  staticFile,
   useCurrentFrame,
   useVideoConfig,
 } from "remotion";
 import { transitionDuration } from "../configuration";
+import { borderRadius, safeSpace } from "../layout/get-layout";
 
 export const TitleCard: React.FC<{
   title: string;
   durationInFrames: number;
 }> = ({ title, durationInFrames }) => {
-  const { fps, width } = useVideoConfig();
+  const { fps, width, height } = useVideoConfig();
   const frame = useCurrentFrame();
 
   const show = (delay: number) =>
@@ -36,32 +41,78 @@ export const TitleCard: React.FC<{
     delay: durationInFrames - transitionDuration,
   });
 
+  const desiredImageWidth = width - safeSpace("square") * 2;
+  const actualImageHeight = Math.min(
+    height * 0.7,
+    (desiredImageWidth / 16) * 9
+  );
+  const actualImageWidth = (actualImageHeight / 9) * 16;
+
   return (
     <AbsoluteFill
       style={{
-        justifyContent: "center",
-        alignItems: "center",
-        translate: interpolate(hide, [0, 1], [0, -width]) + "px 0",
+        translate:
+          interpolate(hide, [0, 1], [0, -width]) +
+          interpolate(show(0), [0, 1], [width, 0]) +
+          "px 0",
       }}
     >
-      <div
+      <Sequence from={6}>
+        <Audio src={staticFile("sounds/whipwhoosh2.mp3")} volume={0.5} />
+      </Sequence>
+      <Sequence from={20}>
+        <Audio src={staticFile("sounds/whipwhoosh.mp3")} volume={0.5} />
+      </Sequence>
+      <AbsoluteFill
         style={{
-          fontFamily: "GT Planar",
-          fontSize: 60,
-          color: "#000",
-          lineHeight: 1.1,
-          fontWeight: "bolder",
-          border: "10px solid black",
-          borderRadius: 20,
-          padding: "15px 40px",
-          display: "inline",
-          background: "#fff",
-          position: "absolute",
-          translate: interpolate(show(0), [0, 1], [width, 0]) + "px",
+          justifyContent: "center",
+          alignItems: "center",
         }}
       >
-        {title}
-      </div>
+        <div
+          style={{
+            overflow: "hidden",
+            borderRadius,
+          }}
+        >
+          <Img
+            style={{
+              width: actualImageWidth,
+              height: actualImageHeight,
+              transform: `scale(${
+                show(6) + interpolate(frame, [0, 100], [0, 0.1])
+              })`,
+            }}
+            src={staticFile("thumbnails/ismillionjslegit.png")}
+          />
+        </div>
+      </AbsoluteFill>
+      <AbsoluteFill
+        style={{
+          justifyContent: "center",
+          alignItems: "center",
+          transform: `translateY(${actualImageHeight / 2}px)`,
+        }}
+      >
+        <div
+          style={{
+            fontFamily: "GT Planar",
+            fontSize: 60,
+            color: "#000",
+            lineHeight: 1.1,
+            fontWeight: "bolder",
+            border: "10px solid black",
+            borderRadius: 20,
+            padding: "15px 40px",
+            display: "inline",
+            background: "#fff",
+            position: "absolute",
+            transform: `scale(${show(20)})`,
+          }}
+        >
+          {title}
+        </div>
+      </AbsoluteFill>
     </AbsoluteFill>
   );
 };
