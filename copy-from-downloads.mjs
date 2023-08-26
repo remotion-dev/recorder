d239e74fb5a3a4e4f0f984638a489a6532d74e1c
Jonny Burger <jonathanburger11@gmail.com>
2023-08-26 14:09:20 +0200

    uber for coding

--- copy-from-downloads.mjs
+++ copy-from-downloads.mjs
@@ -3,7 +3,7 @@ import path from "path";
 import { getSilentParts } from "@remotion/renderer";
 import { execSync } from "child_process";
 
-const prefix = "scope";
+const prefix = "uber-for-coding";
 
 const downloadsDir =
   process.platform === "win32"
