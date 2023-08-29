0838f96f6ae55ff380ce8edfa966018d12e29b35
Jonny Burger <jonathanburger11@gmail.com>
2023-08-29 19:45:36 +0200

    rome story

--- copy-from-downloads.mjs
+++ copy-from-downloads.mjs
@@ -3,7 +3,7 @@ import path from "path";
 import { getSilentParts } from "@remotion/renderer";
 import { execSync } from "child_process";
 
-const prefix = "uber-for-coding";
+const prefix = "rome";
 
 const downloadsDir =
   process.platform === "win32"
