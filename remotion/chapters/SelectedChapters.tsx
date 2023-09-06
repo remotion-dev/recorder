fd3c14f34b22441a77c339c09c346fd7833c5681
Jonny Burger <jonathanburger11@gmail.com>
2023-09-06 11:05:32 +0200

    cool

--- remotion/chapters/SelectedChapters.tsx
+++ remotion/chapters/SelectedChapters.tsx
@@ -63,43 +63,96 @@ export const SelectedChapters: React.FC<{
     (CHAPTER_HEIGHT + CHAPTER_VERTICAL_MARGIN * 2) * shownChapters.length -
     CHAPTER_VERTICAL_MARGIN * 2;
 
-  const styles: React.CSSProperties = useMemo(() => {
-    return {
-      display: "flex",
-      paddingLeft: safeSpace("wide"),
-      paddingTop: safeSpace("wide"),
-    };
-  }, []);
+  const styles = useMemo((): React.CSSProperties => {
+    const selectedChapter = shownChapters.find((c) => c.index === activeIndex);
+    if (!selectedChapter) {
+      throw new Error("Could not find selected chapter");
+    }
+
+    // TODO: Find correct position
+    const pos = selectedChapter.webcamPositions.find(
+      (p) =>
+        p.start - selectedChapter.start <= frame &&
+        p.end - selectedChapter.start > frame
+    );
+    if (!pos) {
+      throw new Error("Could not find position");
+    }
+
+    const { layout, webcamPosition } = pos;
+    console.log(webcamPosition);
+    if (webcamPosition === "top-left") {
+      return {
+        display: "flex",
+        paddingLeft: safeSpace("wide"),
+        paddingTop: safeSpace("wide") * 2 + layout.webcamLayout.height,
+        flex: 1,
+      };
+    }
+
+    if (webcamPosition === "top-right") {
+      return {
+        display: "flex",
+        paddingRight: safeSpace("wide"),
+        alignSelf: "flex-end",
+        flex: 1,
+        paddingTop: safeSpace("wide") * 2 + layout.webcamLayout.height,
+      };
+    }
+
+    if (webcamPosition === "bottom-left") {
+      return {
+        display: "flex",
+        paddingRight: safeSpace("wide"),
+        flex: 1,
+        paddingTop: layout.webcamLayout.y - height - safeSpace("wide"),
+        paddingLeft: safeSpace("wide"),
+      };
+    }
+
+    if (webcamPosition === "bottom-right") {
+      return {
+        display: "flex",
+        paddingRight: safeSpace("wide"),
+        flex: 1,
+        alignSelf: "flex-end",
+        paddingTop: layout.webcamLayout.y - height - safeSpace("wide"),
+      };
+    }
+
+    throw new Error(
+      'chapters for "wide" layout must have a webcam position, not center'
+    );
+  }, [activeIndex, frame, height, shownChapters]);
 
   return (
     <AbsoluteFill
       style={{
-        alignItems: "flex-start",
         transform: `translateX(${translateX}px)`,
-        ...styles,
       }}
     >
       <div
         style={{
           display: "flex",
           flexDirection: "column",
-          alignItems: "flex-start",
           height,
+          ...styles,
         }}
       >
         {shownChapters.map((chapter, i) => {
           return (
-            <WideLayoutChapter
-              key={chapter.id}
-              activeIndex={activeIndex}
-              chapter={chapter}
-              slideY={slideY}
-              slideHighlight={activeIndex > 0}
-              fadeOut={i === 0 && shouldFadeFirstOut}
-              isFirst={i === 0}
-              isLast={i === shownChapters.length - 1}
-              fadeIn={i === shownChapters.length - 1 && shouldFadeLastIn}
-            />
+            <div key={chapter.id}>
+              <WideLayoutChapter
+                activeIndex={activeIndex}
+                chapter={chapter}
+                slideY={slideY}
+                slideHighlight={activeIndex > 0}
+                fadeOut={i === 0 && shouldFadeFirstOut}
+                isFirst={i === 0}
+                isLast={i === shownChapters.length - 1}
+                fadeIn={i === shownChapters.length - 1 && shouldFadeLastIn}
+              />
+            </div>
           );
         })}
       </div>
