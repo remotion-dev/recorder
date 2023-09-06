66aea6d1f42696414ea23fbfb8630819b5ba7348
Jonny Burger <jonathanburger11@gmail.com>
2023-09-06 17:20:22 +0200

    nice

--- remotion/scenes/EndCard.tsx
+++ remotion/scenes/EndCard.tsx
@@ -77,7 +77,7 @@ const IconRow: React.FC<{
       <div
         style={{
           fontSize: 50,
-          fontFamily: "Inter",
+          fontFamily: "GT Planar",
           fontWeight: 700,
           opacity: fadeIn,
         }}
@@ -119,15 +119,6 @@ export const EndCard: React.FC<{
         padding: 90,
       }}
     >
-      <div
-        style={{
-          fontFamily: "Inter",
-          fontSize: 60,
-          fontWeight: 600,
-        }}
-      >
-        Thanks for watching!
-      </div>
       <div style={{ height: 10 }} />
       <div style={{ height: 80 }} />
       <IconRow
@@ -141,7 +132,7 @@ export const EndCard: React.FC<{
         delay={20}
         viewBox="0 0 576 512"
         path="M549.655 124.083c-6.281-23.65-24.787-42.276-48.284-48.597C458.781 64 288 64 288 64S117.22 64 74.629 75.486c-23.497 6.322-42.003 24.947-48.284 48.597-11.412 42.867-11.412 132.305-11.412 132.305s0 89.438 11.412 132.305c6.281 23.65 24.787 41.5 48.284 47.821C117.22 448 288 448 288 448s170.78 0 213.371-11.486c23.497-6.321 42.003-24.171 48.284-47.821 11.412-42.867 11.412-132.305 11.412-132.305s0-89.438-11.412-132.305zm-317.51 213.508V175.185l142.739 81.205-142.739 81.201z"
-        handle={channel === "jonny" ? "/JonnyBurger" : "/@remotion_dev"}
+        handle={channel === "jonny" ? "JonnyBurger" : "@remotion_dev"}
       />
       <div style={{ height: 40 }} />
       <IconRow
@@ -172,7 +163,7 @@ export const EndCard: React.FC<{
         <p
           style={{
             fontSize: 40,
-            fontFamily: "Inter",
+            fontFamily: "GT Planar",
           }}
         >
           Made with Remotion
