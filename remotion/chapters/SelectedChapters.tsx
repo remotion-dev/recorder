6ef9a1315c377c2bf8ce114ef873d14af47a21ce
Jonny Burger <jonathanburger11@gmail.com>
2023-09-06 10:39:52 +0200

    ok

--- remotion/chapters/SelectedChapters.tsx
+++ remotion/chapters/SelectedChapters.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useMemo } from "react";
 import {
   AbsoluteFill,
   interpolate,
@@ -6,7 +6,12 @@ import {
   useCurrentFrame,
   useVideoConfig,
 } from "remotion";
-import { WideLayoutChapter } from "./Chapter";
+import { safeSpace } from "../layout/get-layout";
+import {
+  CHAPTER_HEIGHT,
+  CHAPTER_VERTICAL_MARGIN,
+  WideLayoutChapter,
+} from "./Chapter";
 import type { ChapterType } from "./generate";
 
 export const SelectedChapters: React.FC<{
@@ -54,12 +59,24 @@ export const SelectedChapters: React.FC<{
 
   const translateX = interpolate(jumpIn - jumpOut, [0, 1], [-width, 0]);
 
+  const height =
+    (CHAPTER_HEIGHT + CHAPTER_VERTICAL_MARGIN * 2) * shownChapters.length -
+    CHAPTER_VERTICAL_MARGIN * 2;
+
+  const styles: React.CSSProperties = useMemo(() => {
+    return {
+      display: "flex",
+      paddingLeft: safeSpace("wide"),
+      paddingTop: safeSpace("wide"),
+    };
+  }, []);
+
   return (
     <AbsoluteFill
       style={{
         alignItems: "flex-start",
-        padding: 25,
         transform: `translateX(${translateX}px)`,
+        ...styles,
       }}
     >
       <div
@@ -67,6 +84,7 @@ export const SelectedChapters: React.FC<{
           display: "flex",
           flexDirection: "column",
           alignItems: "flex-start",
+          height,
         }}
       >
         {shownChapters.map((chapter, i) => {
@@ -78,6 +96,8 @@ export const SelectedChapters: React.FC<{
               slideY={slideY}
               slideHighlight={activeIndex > 0}
               fadeOut={i === 0 && shouldFadeFirstOut}
+              isFirst={i === 0}
+              isLast={i === shownChapters.length - 1}
               fadeIn={i === shownChapters.length - 1 && shouldFadeLastIn}
             />
           );
