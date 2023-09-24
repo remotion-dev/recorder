5baf29067e1dd9a09268b28f6c44a2a0084dfeed
Jonny Burger <jonathanburger11@gmail.com>
2023-09-24 11:51:58 +0200

    init davinci

--- copy-from-downloads.mjs
+++ copy-from-downloads.mjs
@@ -3,7 +3,7 @@ import { execSync } from "child_process";
 import fs, { existsSync, mkdirSync } from "fs";
 import path from "path";
 
-const prefix = "bunbun";
+const prefix = "davinci";
 
 const downloadsDir =
   process.platform === "win32"
