f212d607b7b4709adcb2e209aa44207ce82357ce
Jonny Burger <jonathanburger11@gmail.com>
2023-10-18 11:01:50 +0200

    revise end card

---
+++ remotion/scenes/EndCard/index.tsx
@@ -0,0 +1,24 @@
+import { loadFont } from "@remotion/google-fonts/RobotoCondensed";
+import React from "react";
+import { AbsoluteFill } from "remotion";
+import type { CanvasLayout, Channel, Platform } from "../../configuration";
+import { LeftSide } from "./LeftSide";
+
+loadFont();
+
+export const EndCard: React.FC<{
+  channel: Channel;
+  canvasLayout: CanvasLayout;
+  platform: Platform;
+  isTransitioningIn: boolean;
+}> = () => {
+  return (
+    <AbsoluteFill
+      style={{
+        backgroundColor: "white",
+      }}
+    >
+      <LeftSide />
+    </AbsoluteFill>
+  );
+};
