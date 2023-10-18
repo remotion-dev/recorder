f9ea464f71e3d137baf6051dcf634358031ac81c
Jonny Burger <jonathanburger11@gmail.com>
2023-10-18 13:41:48 +0200

    cool

--- remotion/scenes/EndCard/FollowButton.tsx
+++ remotion/scenes/EndCard/FollowButton.tsx
@@ -1,28 +1,6 @@
-import { interpolate, spring, useCurrentFrame, useVideoConfig } from "remotion";
-
 export const followButtonHeight = 140;
 
 export const FollowButton: React.FC = () => {
-  const { fps } = useVideoConfig();
-  const frame = useCurrentFrame();
-
-  const squash = spring({
-    fps,
-    frame,
-    durationInFrames: 40,
-    delay: 30,
-  });
-
-  const scaleX =
-    interpolate(squash, [1, 2], [1, 1.1], {
-      extrapolateLeft: "clamp",
-    }) +
-    Math.max(squash, 1) -
-    1;
-  const scaleY = interpolate(squash, [1, 2], [1, 0.3], {
-    extrapolateLeft: "clamp",
-  });
-
   return (
     <div
       style={{
@@ -37,7 +15,6 @@ export const FollowButton: React.FC = () => {
         fontFamily: "GT Planar",
         fontSize: 50,
         fontWeight: 500,
-        transform: `scale(${scaleX}, ${scaleY})`,
       }}
     >
       Follow
--- remotion/scenes/EndCard/LeftSide.tsx
+++ remotion/scenes/EndCard/LeftSide.tsx
@@ -1,5 +1,15 @@
-import React from "react";
-import { AbsoluteFill, Img } from "remotion";
+import React, { useEffect, useRef, useState } from "react";
+import {
+  AbsoluteFill,
+  continueRender,
+  delayRender,
+  Img,
+  interpolate,
+  spring,
+  useCurrentFrame,
+  useVideoConfig,
+} from "remotion";
+import { transitionDuration } from "../../configuration";
 import { FollowButton, followButtonHeight } from "./FollowButton";
 import {
   InstagramIcon,
@@ -66,9 +76,16 @@ const labelStyle: React.CSSProperties = {
 const IconRow: React.FC<{
   type: "youtube" | "linkedin" | "instagram" | "x" | "link";
   label: string;
-}> = ({ type, label }) => {
+  fadeInDelay: number;
+}> = ({ type, label, fadeInDelay }) => {
+  const frame = useCurrentFrame();
+  const opacity = interpolate(frame, [fadeInDelay, fadeInDelay + 10], [0, 1], {
+    extrapolateRight: "clamp",
+    extrapolateLeft: "clamp",
+  });
+
   return (
-    <div style={iconRow}>
+    <div style={{ ...iconRow, opacity }}>
       <div style={iconContainer}>
         {type === "link" ? <LinkIcon height={60} /> : null}
         {type === "youtube" ? <YouTubeIcon height={60} /> : null}
@@ -83,6 +100,47 @@ const IconRow: React.FC<{
 };
 
 export const LeftSide: React.FC<{}> = () => {
+  const ref = useRef<HTMLDivElement>(null);
+  const scaler = useRef<HTMLDivElement>(null);
+  const [handle] = useState(() => delayRender());
+  const [contentHeight, setHeight] = useState(0);
+
+  const { fps } = useVideoConfig();
+  const frame = useCurrentFrame();
+
+  const slideDelay = transitionDuration + 20;
+
+  const slideUp = spring({
+    fps,
+    frame,
+    config: {
+      damping: 200,
+    },
+    delay: transitionDuration + 20,
+  });
+
+  useEffect(() => {
+    const calc = () => {
+      window.requestAnimationFrame(() => {
+        const height = ref.current?.getBoundingClientRect().height as number;
+        if (height === 0) {
+          calc();
+          return;
+        }
+
+        const scale =
+          (scaler.current?.getBoundingClientRect().height as number) / 100;
+
+        const scaled = height / scale;
+
+        setHeight(scaled);
+        continueRender(handle);
+      });
+    };
+
+    calc();
+  }, [handle]);
+
   return (
     <AbsoluteFill
       style={{
@@ -90,13 +148,45 @@ export const LeftSide: React.FC<{}> = () => {
         justifyContent: "center",
       }}
     >
-      <FollowCTA />
-      <div style={{ height: 80 }} />
-      <IconRow type="youtube" label="/JonnyBurger" />
-      <IconRow type="linkedin" label="Jonny Burger" />
-      <div style={{ height: 80 }} />
-      <IconRow type="link" label="remotion.pro" />
-      <IconRow type="link" label="remotion.dev/shapes" />
+      <div
+        ref={scaler}
+        style={{ height: 100, position: "absolute", top: -100000 }}
+      />
+      <div
+        style={{
+          transform: `translateY(${interpolate(
+            slideUp,
+            [0, 1],
+            [contentHeight / 2, 0],
+          )}px)`,
+        }}
+      >
+        <FollowCTA />
+      </div>
+      <div ref={ref}>
+        <div style={{ height: 80 }} />
+        <IconRow
+          fadeInDelay={slideDelay + 9}
+          type="youtube"
+          label="/JonnyBurger"
+        />
+        <IconRow
+          fadeInDelay={slideDelay + 6}
+          type="linkedin"
+          label="Jonny Burger"
+        />
+        <div style={{ height: 80 }} />
+        <IconRow
+          fadeInDelay={slideDelay + 3}
+          type="link"
+          label="remotion.pro"
+        />
+        <IconRow
+          fadeInDelay={slideDelay}
+          type="link"
+          label="remotion.dev/shapes"
+        />
+      </div>
     </AbsoluteFill>
   );
 };
