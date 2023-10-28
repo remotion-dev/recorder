755270407ce687f13165b81012f3d7b10d41cd29
Jonny Burger <jonathanburger11@gmail.com>
2023-10-28 11:38:45 +0200

    components

--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,16 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
+import { ThemeProvider } from "./components/theme-provider";
 import { DevicePermission } from "./DevicePermission";
 import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
-    <DevicePermission>
-      <App />
-    </DevicePermission>
+    <ThemeProvider defaultTheme="dark">
+      <DevicePermission>
+        <App />
+      </DevicePermission>
+    </ThemeProvider>
   </React.StrictMode>,
 );
