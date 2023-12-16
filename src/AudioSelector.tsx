34072f290f4ff80629c5f37dbb696e1ed46f73d1
Jonny Burger <jonathanburger11@gmail.com>
2023-12-16 18:06:07 +0100

    costs

--- src/AudioSelector.tsx
+++ src/AudioSelector.tsx
@@ -43,6 +43,7 @@ export const AudioSelector: React.FC<{
         {devices
           .filter((d) => d.kind === "audioinput")
           .map((d) => {
+            console.log(d);
             const label = getDeviceLabel(d);
             return (
               <SelectItem key={d.deviceId} value={d.deviceId}>
