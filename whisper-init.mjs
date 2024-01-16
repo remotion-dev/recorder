32cbbb580142960defcf43964c4c72cb48b3c11c
Jonny Burger <jonathanburger11@gmail.com>
2024-01-16 15:49:43 +0100

    download for windows

--- whisper-init.mjs
+++ whisper-init.mjs
@@ -1,6 +1,8 @@
 import fs from "fs";
 import { execSync } from "node:child_process";
 import { existsSync } from "node:fs";
+import { Readable } from "node:stream";
+import { finished } from "node:stream/promises";
 import path from "path";
 
 const installWhisperForMacOS = () => {
@@ -33,46 +35,34 @@ const downloadWindowsBinary = async () => {
 
   const { body } = await fetch(url);
   await finished(Readable.fromWeb(body).pipe(fileStream));
+
+  execSync(
+    `Expand-Archive -Force ${filePath} ${path.join(
+      process.cwd(),
+      "whisper-bin-x64",
+    )}`,
+    { shell: "powershell" },
+  );
 };
 
 const downloadWhisperBaseModel = async () => {
   const baseModelUrl =
     "https://huggingface.co/ggerganov/whisper.cpp/resolve/main/ggml-base.en.bin";
 
-  const res = await fetch(baseModelUrl);
-
-  if (!res.ok) {
-    throw new Error(
-      `Failed to download whisper base model. Status code ${res.status}`,
-    );
-  }
-
-  const filePath = path.join(process.cwd(), "whisper-bin-x64", "models");
-
-  fs.mkdir(filePath, (err) => {
+  const dirPath = path.join(process.cwd(), "whisper-bin-x64", "models");
+  fs.mkdir(dirPath, (err) => {
     if (err) {
       return console.error(err);
     }
   });
 
-  // create models directory
+  const filePath = path.join(dirPath, "ggml-base.en.bin");
   const fileStream = fs.createWriteStream(filePath);
 
-  return new Promise((resolve, reject) => {
-    res.body.pipe(fileStream);
+  const { body } = await fetch(baseModelUrl);
+  await finished(Readable.fromWeb(body).pipe(fileStream));
 
-    res.body.on("error", (err) => {
-      reject(err);
-    });
-
-    fileStream.on("finish", () => {
-      resolve();
-    });
-
-    fileStream.on("error", (err) => {
-      reject(err);
-    });
-  });
+  // create models directory
 };
 
 const installWhisperForWindows = async () => {
