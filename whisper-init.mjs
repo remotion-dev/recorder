6cac914702c57c3332ae7aa29ada95158862278a
Patric Salvisberg <patric.salvisberg@gmail.com>
2024-01-17 11:10:05 +0100

    ask for permission

--- whisper-init.mjs
+++ whisper-init.mjs
@@ -1,31 +1,12 @@
-import fs from "fs";
+import fs, { existsSync } from "fs";
 import { execSync } from "node:child_process";
-import { existsSync } from "node:fs";
+import { createInterface } from "node:readline";
 import { Readable } from "node:stream";
 import { finished } from "node:stream/promises";
 import path from "path";
 
-const installWhisperForMacOS = () => {
-  console.log("Installing whisper. This may take a while...");
-  execSync(`git clone https://github.com/ggerganov/whisper.cpp.git 2>&1`);
+const ac = new AbortController();
 
-  execSync(`make`, {
-    cwd: path.join(process.cwd(), "whisper.cpp"),
-  });
-
-  // check if base model for english exists
-  console.log("Checking whisper models...");
-  if (
-    !existsSync(path.join(process.cwd(), "whisper.cpp/models/ggml-base.en.bin"))
-  ) {
-    console.log("Downloading english base model...");
-    execSync(` bash ./models/download-ggml-model.sh base.en`, {
-      cwd: path.join(process.cwd(), "whisper.cpp"),
-    });
-  }
-};
-
-// TODO: refactor for less redundnacy
 const downloadWindowsBinary = async () => {
   const url =
     "https://github.com/ggerganov/whisper.cpp/releases/download/v1.5.4/whisper-bin-x64.zip";
@@ -41,7 +22,7 @@ const downloadWindowsBinary = async () => {
       process.cwd(),
       "whisper-bin-x64",
     )}`,
-    { shell: "powershell" },
+    { shell: "powershell", stdio: "inherit" },
   );
 };
 
@@ -61,8 +42,31 @@ const downloadWhisperBaseModel = async () => {
 
   const { body } = await fetch(baseModelUrl);
   await finished(Readable.fromWeb(body).pipe(fileStream));
-
   // create models directory
+};
+
+const installWhisperForMacOS = () => {
+  console.log("Installing whisper. This may take a while...");
+  execSync(`git clone https://github.com/ggerganov/whisper.cpp.git 2>&1`, {
+    stdio: "inherit",
+  });
+
+  execSync(`make`, {
+    cwd: path.join(process.cwd(), "whisper.cpp"),
+    stdio: "inherit",
+  });
+
+  // check if base model for english exists
+  console.log("Checking whisper models...");
+  if (
+    !existsSync(path.join(process.cwd(), "whisper.cpp/models/ggml-base.en.bin"))
+  ) {
+    console.log("Downloading english base model...");
+    execSync(` bash ./models/download-ggml-model.sh base.en`, {
+      cwd: path.join(process.cwd(), "whisper.cpp"),
+      stdio: "inherit",
+    });
+  }
 };
 
 const installWhisperForWindows = async () => {
@@ -70,16 +74,49 @@ const installWhisperForWindows = async () => {
   await downloadWhisperBaseModel();
 };
 
-if (process.platform === "darwin") {
-  installWhisperForMacOS();
-}
+const startInstallation = async () => {
+  if (process.platform === "darwin" || process.platform === "linux") {
+    installWhisperForMacOS();
+    console.log("after install");
+  }
 
-if (process.platform === "win32") {
-  installWhisperForWindows()
-    .then(() => {
-      console.log("Download successful");
-    })
-    .catch((e) => {
-      console.error("An error occured while downloading: ", e);
-    });
-}
+  if (process.platform === "win32") {
+    await installWhisperForWindows();
+    console.log("Download successful");
+  }
+};
+
+const askForPermission = async () => {
+  const readline = createInterface({
+    input: process.stdin,
+    output: process.stdout,
+  });
+
+  const answer = await new Promise((resolve, reject) => {
+    const { signal } = ac;
+    const onAbort = () => {
+      reject(new Error("aborted"));
+    };
+
+    signal.addEventListener("abort", onAbort, { once: true });
+
+    readline.question(
+      "Remotion Recorder would like to install whisper.cpp on your computer. Do you want to proceed? (y/n)",
+      { signal },
+      (userInput) => {
+        signal.removeEventListener("abort", onAbort);
+        resolve(userInput);
+        readline.close();
+      },
+    );
+  });
+
+  if (answer === "y") {
+    console.log("starting installation...");
+    await startInstallation();
+  } else {
+    console.log("installation aborted");
+  }
+};
+
+await askForPermission();
