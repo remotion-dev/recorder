dd29011a8f2d5c4c340dff9ac063ae48aaa7021a
Jonny Burger <jonathanburger11@gmail.com>
2024-03-22 12:02:09 +0100

    update script

---
+++ scripts/captions/caption-file.ts
@@ -0,0 +1,60 @@
+import { transcribe } from "@remotion/install-whisper-cpp";
+import { execSync } from "child_process";
+import { existsSync, mkdirSync, rmSync, writeFileSync } from "fs";
+import path from "path";
+import { WHISPER_MODEL, WHISPER_PATH } from "./install-whisper";
+
+const extractToTempAudioFile = (
+  fileToTranscribe: string,
+  tempOutFile: string,
+) => {
+  // extracting audio from mp4 and save it as 16khz wav file
+  execSync(
+    `npx remotion ffmpeg -i ${fileToTranscribe} -ar 16000 ${tempOutFile}`,
+  );
+};
+
+export const captionFile = async ({
+  file,
+  folder,
+  fileToTranscribe,
+}: {
+  file: string;
+  folder: string;
+  fileToTranscribe: string;
+}) => {
+  const fileName = file.split(".")[0] + ".wav";
+
+  const isTranscribed = existsSync(
+    fileToTranscribe.replace(".mp4", ".json").replace("webcam", "subs"),
+  );
+  if (isTranscribed) {
+    return;
+  }
+
+  if (!existsSync(path.join(process.cwd(), "temp"))) {
+    mkdirSync("temp");
+  }
+
+  const wavFile = path.join(process.cwd(), `temp/${fileName}`);
+  extractToTempAudioFile(fileToTranscribe, wavFile);
+
+  // defining the output file location and name
+  const outPath = path
+    .join(
+      process.cwd(),
+      `public/${folder}/${fileName.replace(".wav", ".json")}`,
+    )
+    .replace("webcam", "subs");
+
+  const { transcription } = await transcribe({
+    inputPath: wavFile,
+    model: WHISPER_MODEL,
+    tokenLevelTimestamps: true,
+    whisperPath: WHISPER_PATH,
+    translateToEnglish: false,
+  });
+
+  rmSync(wavFile);
+  writeFileSync(outPath, JSON.stringify(transcription, null, 2));
+};
---
+++ sub.ts
@@ -0,0 +1,29 @@
+import { lstatSync, readdirSync } from "node:fs";
+import path from "path";
+import { captionFile } from "./scripts/captions/caption-file";
+import { ensureWhisper } from "./scripts/captions/install-whisper";
+
+await ensureWhisper();
+
+const publicFolder = path.join(process.cwd(), "public");
+const folders = readdirSync(publicFolder).filter((f) => f !== ".DS_Store");
+
+for (const folder of folders) {
+  if (!lstatSync(path.join(publicFolder, folder)).isDirectory()) {
+    continue;
+  }
+
+  const files = readdirSync(path.join(publicFolder, folder)).filter(
+    (f) => f !== ".DS_Store",
+  );
+
+  for (const file of files) {
+    if (!file.startsWith("webcam")) {
+      continue;
+    }
+
+    const fileToTranscribe = path.join(publicFolder, folder, file);
+
+    await captionFile({ file, folder, fileToTranscribe });
+  }
+}
