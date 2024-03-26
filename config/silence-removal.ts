ffdaab9fffecdb5655f06fcd731604a1e7a004bc
Jonny Burger <jonathanburger11@gmail.com>
2024-03-26 16:17:31 +0100

    silence removal

---
+++ config/silence-removal.ts
@@ -0,0 +1,2 @@
+export const MIN_DURATION_IN_SECONDS = 0.3;
+export const PADDING_IN_SECONDS = 0.25;
--- copy.ts
+++ copy.ts
@@ -1,20 +1,16 @@
-import { getSilentParts } from "@remotion/renderer";
-import { execSync } from "child_process";
-import fs, { existsSync, mkdirSync } from "fs";
-import path from "path";
-import { DISPLAY_PREFIX, WEBCAM_PREFIX } from "./config/cameras";
+import fs from "fs";
+import { WEBCAM_PREFIX } from "./config/cameras";
 import { getDownloadsFolder } from "./scripts/get-downloads-folder";
+import { removeSilence } from "./scripts/remove-silence";
 
 const prefix = "empty";
 
 const downloadsDir = getDownloadsFolder();
-
 const filesFromDownloads = fs.readdirSync(downloadsDir);
 
 const webcam = filesFromDownloads.filter((file) =>
   file.startsWith(WEBCAM_PREFIX),
 );
-
 const sorted = webcam.sort((a, b) => {
   const timestampA = Number(a.match(/([0-9]+)/)![1]);
   const timestampB = Number(b.match(/([0-9]+)/)![1]);
@@ -25,59 +21,6 @@ const sorted = webcam.sort((a, b) => {
 const latest = sorted[0];
 const latestTimestamp = Number(latest.match(/([0-9]+)/)![1]);
 
-const folder = `public${path.sep}${prefix}`;
+await removeSilence(latestTimestamp, prefix);
 
-const displayLatest = `${DISPLAY_PREFIX}${latestTimestamp}.webm`;
-const webcamLatest = `${WEBCAM_PREFIX}${latestTimestamp}.webm`;
-
-const displaySrc = path.join(downloadsDir, displayLatest);
-const webcamSrc = path.join(downloadsDir, webcamLatest);
-
-mkdirSync(folder, { recursive: true });
-
-const { audibleParts } = await getSilentParts({
-  src: webcamSrc,
-  minDurationInSeconds: 0.3,
-});
-
-const padding = 0.25;
-
-const ffmpegTrim =
-  audibleParts.length > 0
-    ? [
-        "-ss",
-        audibleParts[0].startInSeconds - padding,
-        "-to",
-        audibleParts[audibleParts.length - 1].endInSeconds + padding * 2,
-      ]
-    : [];
-
-const convert = (i: string, o: string) => {
-  execSync(
-    [
-      "npx remotion ffmpeg",
-      ...ffmpegTrim,
-      "-i",
-      i,
-      "-movflags",
-      "+faststart",
-      "-r",
-      "30",
-      "-y",
-      o,
-    ].join(" "),
-    {
-      stdio: "inherit",
-    },
-  );
-};
-
-if (existsSync(displaySrc)) {
-  convert(
-    displaySrc,
-    path.join(folder, displayLatest.replace(".webm", ".mp4")),
-  );
-}
-
-convert(webcamSrc, path.join(folder, webcamLatest.replace(".webm", ".mp4")));
-console.log("copied", latest, displayLatest);
+console.log("Copied", latest);
