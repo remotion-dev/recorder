47c7862011f95994ed3682fca5dc038af0d4c2fc
Jonny Burger <jonathanburger11@gmail.com>
2024-03-26 15:32:02 +0100

    new scripts

--- copy.ts
+++ copy.ts
@@ -2,33 +2,33 @@ import { getSilentParts } from "@remotion/renderer";
 import { execSync } from "child_process";
 import fs, { existsSync, mkdirSync } from "fs";
 import path from "path";
-import { getDownloadsFolder } from "./src/get-downloads-folder.js";
+import { getDownloadsFolder } from "./scripts/get-downloads-folder";
 
-const prefix = "january";
+const prefix = "empty";
 
 const downloadsDir = getDownloadsFolder();
 
-const filesFromDownloads = await fs.readdirSync(downloadsDir);
+const filesFromDownloads = fs.readdirSync(downloadsDir);
 
 const webcam = filesFromDownloads.filter((file) => file.includes("webcam"));
 const sorted = webcam.sort((a, b) => {
-  const timestampA = Number(a.match(/([0-9]+)/)[1]);
-  const timestampB = Number(b.match(/([0-9]+)/)[1]);
+  const timestampA = Number(a.match(/([0-9]+)/)![1]);
+  const timestampB = Number(b.match(/([0-9]+)/)![1]);
 
   return timestampB - timestampA;
 });
 
 const latest = sorted[0];
-const latestTimestamp = Number(latest.match(/([0-9]+)/)[1]);
+const latestTimestamp = Number(latest.match(/([0-9]+)/)![1]);
 
 const folder = `public${path.sep}${prefix}`;
 
 const displayLatest = `display${latestTimestamp}.webm`;
 const webcamLatest = `webcam${latestTimestamp}.webm`;
 
-const displaySrc = `${downloadsDir}${path.sep}${displayLatest}`;
+const displaySrc = path.join(downloadsDir, displayLatest);
 
-const webcamSrc = `${downloadsDir}${path.sep}${webcamLatest}`;
+const webcamSrc = path.join(downloadsDir, webcamLatest);
 
 mkdirSync(folder, { recursive: true });
 
@@ -49,7 +49,7 @@ const ffmpegTrim =
       ]
     : [];
 
-const convert = (i, o) => {
+const convert = (i: string, o: string) => {
   execSync(
     [
       "npx remotion ffmpeg",
@@ -72,12 +72,9 @@ const convert = (i, o) => {
 if (existsSync(displaySrc)) {
   convert(
     displaySrc,
-    `${folder}${path.sep}${displayLatest.replace(".webm", ".mp4")}`,
+    path.join(folder, displayLatest.replace(".webm", ".mp4")),
   );
 }
 
-convert(
-  webcamSrc,
-  `${folder}${path.sep}${webcamLatest.replace(".webm", ".mp4")}`,
-);
+convert(webcamSrc, path.join(folder, webcamLatest.replace(".webm", ".mp4")));
 console.log("copied", latest, displayLatest);
