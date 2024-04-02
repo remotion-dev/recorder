b3632d18ce2e824a9b03eff467c5c26cb47f2aa4
Jonny Burger <jonathanburger11@gmail.com>
2024-04-02 11:40:19 +0200

    wait for fonts

--- config/fonts.ts
+++ config/fonts.ts
@@ -8,8 +8,13 @@ import {
   loadFont as loadMonospace,
 } from "@remotion/google-fonts/RobotoCondensed";
 
-loadRegular();
-loadMonospace();
+const regular = loadRegular();
+const monospace = loadMonospace();
+
+export const waitForFonts = async () => {
+  await regular.waitUntilDone();
+  await monospace.waitUntilDone();
+};
 
 export const REGULAR_FONT_FAMILY = regularFont;
 export const REGULAR_FONT_WEIGHT = 600;
