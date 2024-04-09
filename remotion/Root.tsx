c9db2432261f1b74cd86b5ab0969aa194808806a
Patric Salvisberg <patric.salvisberg@gmail.com>
2024-04-09 16:43:48 +0200

    spaghetti

--- remotion/Root.tsx
+++ remotion/Root.tsx
@@ -30,6 +30,30 @@ export const Root = () => {
         }}
         calculateMetadata={calcMetadata}
       />
+
+      <Composition
+        component={Main}
+        id="newone"
+        schema={videoConf}
+        defaultProps={{
+          theme: "light" as const,
+          canvasLayout: "square" as const,
+          scenes: [
+            {
+              type: "videoscene" as const,
+              webcamPosition: "previous" as const,
+              trimStart: 0,
+              duration: null,
+              transitionToNextScene: true,
+              newChapter: "",
+              stopChapteringAfterThis: false,
+              music: "previous" as const,
+            },
+          ],
+          scenesAndMetadata: [],
+        }}
+        calculateMetadata={calcMetadata}
+      />
     </>
   );
 };
