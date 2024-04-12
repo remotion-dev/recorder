8a9599fb90a4b280941a779f5c0f957eeff7e783
Patric Salvisberg <patric.salvisberg@gmail.com>
2024-04-12 14:48:30 +0200

    project creating from recorder

---
+++ src/create-project.ts
@@ -0,0 +1,27 @@
+import { SERVER_PORT } from "../config/server";
+
+type CreateProjectResBodyType = {
+  success: boolean;
+  message: string;
+};
+
+export const createProject = async (projectName: string) => {
+  const payload = { projectName };
+  const res = await fetch(
+    `http://localhost:${SERVER_PORT}/api/create-project`,
+    {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify(payload),
+    },
+  );
+
+  const jsn = (await res.json()) as CreateProjectResBodyType;
+  console.log(jsn);
+
+  if (res.status === 201) {
+    return jsn;
+  }
+
+  throw new Error(jsn.message);
+};
