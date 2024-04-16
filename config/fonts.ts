311450c907a18793c0e14e32079cb566969cfdc4
Jonny Burger <jonathanburger11@gmail.com>
2024-04-16 07:57:04 +0200

    Update fonts.ts

--- config/fonts.ts
+++ config/fonts.ts
@@ -23,4 +23,4 @@ export const MONOSPACE_FONT_FAMILY = monospaceFont;
 export const MONOSPACE_FONT_WEIGHT = 500;
 
 export const TITLE_FONT_FAMILY = regularFont;
-export const TITLE_FONT_WEIGHT = 500;
+export const TITLE_FONT_WEIGHT = 600;
