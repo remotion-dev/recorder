686f22762b4b68bd839ab50b83fbc15c3d19da3f
Jonny Burger <jonathanburger11@gmail.com>
2024-04-16 07:47:01 +0200

    fix whisper inaccuracy
    Fixes #15

--- config/whisper.ts
+++ config/whisper.ts
@@ -3,3 +3,5 @@ import path from "path";
 
 export const WHISPER_PATH = path.join(process.cwd(), "whisper.cpp");
 export const WHISPER_MODEL: WhisperModel = "medium.en";
+// The git reference, can be a commit, branch or tag
+export const WHISPER_REF = "9fab28135c7867bb7eccd9ebcd2ea8d52e42ca81";
