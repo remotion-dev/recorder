6ffe29886861f0b65bfe4733eb146cb80b6dac87
Nico Martin <mail@nico.dev>
2024-05-28 22:49:56 +0200

    added video size select option

--- src/helpers/get-selected-video-source.ts
+++ src/helpers/get-selected-video-source.ts
@@ -9,12 +9,29 @@ export type SelectedSource =
       type: "display";
     };
 
+export enum VideoSize {
+  "4K" = "4k",
+  "Full HD" = "FullHD",
+  HD = "HD",
+  SD = "SD",
+}
+
+export const VIDEO_SIZES: Record<VideoSize, { width: number; height: number }> =
+  {
+    [VideoSize["4K"]]: { width: 3840, height: 2160 },
+    [VideoSize["Full HD"]]: { width: 1920, height: 1080 },
+    [VideoSize.HD]: { width: 1280, height: 720 },
+    [VideoSize.SD]: { width: 640, height: 480 },
+  };
+
 export const getSelectedVideoSource = ({
   value,
   devices,
+  size = VideoSize.HD,
 }: {
   value: string;
   devices: MediaDeviceInfo[];
+  size?: VideoSize;
 }): SelectedSource | null => {
   if (value === "undefined") {
     return null;
@@ -27,11 +44,20 @@ export const getSelectedVideoSource = ({
     device instanceof InputDeviceInfo
   ) {
     const capabilities = device.getCapabilities();
+    const width = capabilities.width?.max ?? null;
+    const height = capabilities.height?.max ?? null;
+
     return {
       type: "camera",
       deviceId: device.deviceId,
-      maxWidth: capabilities.width?.max ?? null,
-      maxHeight: capabilities.height?.max ?? null,
+      maxWidth:
+        !width || width < VIDEO_SIZES[size].width
+          ? width
+          : VIDEO_SIZES[size].width,
+      maxHeight:
+        !height || height < VIDEO_SIZES[size].height
+          ? height
+          : VIDEO_SIZES[size].height,
     };
   }
 
