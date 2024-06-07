9ee0e267db64267aff27a2d3ccc4e586132b2898
Jonny Burger <jonathanburger11@gmail.com>
2024-06-07 11:53:10 +0200

    fix volume meter

--- src/components/VolumeMeter.tsx
+++ src/components/VolumeMeter.tsx
@@ -26,46 +26,49 @@ export const VolumeMeter: React.FC<{
   const [currentVolume, setCurrentVolume] = useState(0);
 
   useEffect(() => {
-    const getVolume = () => {
-      if (!mediaStream || mediaStream.getAudioTracks().length === 0) {
-        return 0;
-      }
+    if (!mediaStream || mediaStream.getAudioTracks().length === 0) {
+      return;
+    }
 
-      const audioContext = new AudioContext();
-      const analyser = audioContext.createAnalyser();
-      const microphone = audioContext.createMediaStreamSource(mediaStream);
-      const scriptProcessor = audioContext.createScriptProcessor(512, 1, 1); // (bufferSize, inputChannels, outputChannels).
+    const audioContext = new AudioContext();
+    const analyser = audioContext.createAnalyser();
+    const microphone = audioContext.createMediaStreamSource(mediaStream);
+    const scriptProcessor = audioContext.createScriptProcessor(512, 1, 1); // (bufferSize, inputChannels, outputChannels).
 
-      analyser.smoothingTimeConstant = 0.9;
-      // An unsigned integer, representing the window size of the FFT, given in number of samples.
-      // A higher value will result in more details in the frequency domain but fewer details in the amplitude domain.
-      analyser.fftSize = 512; // FftSize 512 and minDecibels -123 seems to work pretty similar to OBS volume meter
-      analyser.minDecibels = -123;
-      analyser.maxDecibels = 0;
+    analyser.smoothingTimeConstant = 0.9;
+    // An unsigned integer, representing the window size of the FFT, given in number of samples.
+    // A higher value will result in more details in the frequency domain but fewer details in the amplitude domain.
+    analyser.fftSize = 512; // FftSize 512 and minDecibels -123 seems to work pretty similar to OBS volume meter
+    analyser.minDecibels = -123;
+    analyser.maxDecibels = 0;
 
-      microphone.connect(analyser);
-      analyser.connect(scriptProcessor);
-      scriptProcessor.connect(audioContext.destination);
-      scriptProcessor.onaudioprocess = () => {
-        const array = new Uint8Array(analyser.frequencyBinCount);
-        // Copies frequency data into array. Frequency data is composed of integers between 0 and 255, representing the volume of each frequency.
-        // Every item in the array represents the decibel value of a specific frequency.
-        analyser.getByteFrequencyData(array);
-        // Get the sum of all decibel values of each frequency in the array
-        const arraySum = array.reduce((a, value) => a + value, 0);
-        let currentAverageVolume = 0;
-        // Get the average volume by dividing the sum by the number of items in the array
-        currentAverageVolume = arraySum / array.length;
-        // Extra smoothing
-        setCurrentVolume((old) => (old + currentAverageVolume) / 2);
-      };
+    microphone.connect(analyser);
+    analyser.connect(scriptProcessor);
+    scriptProcessor.connect(audioContext.destination);
+    const onAudioProcess = () => {
+      const array = new Uint8Array(analyser.frequencyBinCount);
+      // Copies frequency data into array. Frequency data is composed of integers between 0 and 255, representing the volume of each frequency.
+      // Every item in the array represents the decibel value of a specific frequency.
+      analyser.getByteFrequencyData(array);
+      // Get the sum of all decibel values of each frequency in the array
+      const arraySum = array.reduce((a, value) => a + value, 0);
+      let currentAverageVolume = 0;
+      // Get the average volume by dividing the sum by the number of items in the array
+      currentAverageVolume = arraySum / array.length;
+      // Extra smoothing
+      setCurrentVolume((old) => (old + currentAverageVolume) / 2);
     };
 
-    getVolume();
+    scriptProcessor.addEventListener("audioprocess", onAudioProcess);
+
+    return () => {
+      scriptProcessor.removeEventListener("audioprocess", onAudioProcess);
+      scriptProcessor.disconnect();
+    };
   }, [mediaStream]);
 
   const widthPercent = useMemo(() => {
-    return currentVolume < 100 ? 100 - currentVolume : 0;
+    return currentVolume < 100 ? 85 - currentVolume : 0;
   }, [currentVolume]);
 
   const dynamicCover: React.CSSProperties = useMemo(() => {
