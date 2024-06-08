5fb14544663f9c39485ca804faa4579fd90004fd
Jonny Burger <jonathanburger11@gmail.com>
2024-06-08 14:23:43 +0200

    Ensure script is being run with Bun

--- studio.ts
+++ studio.ts
@@ -1,5 +1,10 @@
 import { startStudioAndServer } from "./scripts/server";
 
+if (typeof Bun === "undefined") {
+  console.log("Run this script with Bun: `bun studio.ts`");
+  process.exit(1);
+}
+
 if (
   typeof Bun !== "undefined" &&
   !Bun.semver.satisfies(Bun.version, ">=1.1.11")
