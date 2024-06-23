e8b5a4d66a9f0e48ec2e6d74cd2f7cffbe3e38a4
Jonny Burger <jonathanburger11@gmail.com>
2024-06-23 13:13:09 +0200

    remove from welcome

--- public/welcome/code1711527094922.tsx
+++
@@ -1,9 +0,0 @@
-type Props = {
-  onClick: () => void;
-  onDblClick: () => void;
-};
-
-// @errors: 2741
-const props: Props = {
-  onClick: () => {},
-};
--- public/welcome/code1711527196961.tsx
+++
@@ -1,9 +0,0 @@
-type Props = {
-  onClick: () => void;
-  onDblClick: () => void;
-};
-
-const props: Props = {
-  onClick: () => {},
-  onDblClick: () => {},
-};
