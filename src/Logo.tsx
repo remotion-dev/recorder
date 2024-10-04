54a6ce282b2ab826551d4e87cb267abc86a4842d
Jonny Burger <jonathanburger11@gmail.com>
2024-10-04 09:54:34 +0200

    new logo and background

--- src/Logo.tsx
+++ src/Logo.tsx
@@ -1,8 +1,8 @@
 import React from "react";
 
 const style: React.CSSProperties = {
-  height: 40,
-  width: 127,
+  height: 48 / 1.8,
+  width: 246 / 1.8,
 };
 
 export const Logo: React.FC = () => {
