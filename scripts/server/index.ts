ac07eeac727417c1476a56b4c9b209253fa69a33
Jonny Burger <jonathanburger11@gmail.com>
2025-03-13 16:35:42 +0100

    don't start two tabs when launching

--- scripts/server/index.ts
+++ scripts/server/index.ts
@@ -1,15 +1,9 @@
-import { StudioServerInternals } from "@remotion/studio-server";
 import { spawn } from "child_process";
-import { SERVER_PORT } from "../../config/server";
 import { startServer } from "./express-server";
 
 export const startStudioAndServer = async () => {
   await startServer();
-  StudioServerInternals.openBrowser({
-    url: `http://localhost:${SERVER_PORT}`,
-    browserArgs: undefined,
-    browserFlag: undefined,
-  });
+
   const bunxProcess = spawn("bun", ["x", "remotion", "studio"], {
     stdio: "inherit",
     shell: process.platform === "win32" ? "cmd.exe" : undefined,
